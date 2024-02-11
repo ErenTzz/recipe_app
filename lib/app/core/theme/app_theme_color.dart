5c327feb8047bc05a2de3361fd1e57dc7e85317f
Eren Toksöz <eren.toksoz@hotmail.com>
2024-02-11 23:07:18 +0300

    feat: added AppThemeColor in App

---
+++ lib/app/core/theme/app_theme_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppThemeColor {
+  AppThemeColor._();
+
+  static const primary = Color(0xff1FCC79);
+  static const secondary = Color(0xffFF6464);
+  static const mainText = Color(0xff2E3E5C);
+  static const secondaryText = Color(0xff9FA5C0);
+  static const outline = Color(0xffD0DBEA);
+  static const form = Color(0xffF4F5F7);
+  static const white = Color(0xffFFFFFF);
+}
